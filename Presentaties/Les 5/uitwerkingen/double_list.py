7e415d752c6892bc3cfe91d8efa3a87e0bba0e22
Ted de Gouw <ted@hastur.nl>
2024-03-09 08:57:52 +0100

    presentaties verwijderd
    presentaties verwijderd vanwege veranderd lesprogramma.

--- Presentaties/Les 5/uitwerkingen/double_list.py
+++
@@ -1,9 +0,0 @@
-def double_list(input_list):
-    return [x * 2 for x in input_list]
-
-# Test de functie
-numbers = [1, 2, 3, 4, 5]
-doubled_numbers = double_list(numbers)
-print("Originele lijst:", numbers)
-print("Verdubbelde lijst:", doubled_numbers)
-
--- Presentaties/Les 5/uitwerkingen/immutabiliteit.py
+++
@@ -1,7 +0,0 @@
-# Gebruik tuple voor onveranderlijke gegevensstructuur
-original_names = ('Alice', 'Bob', 'Charlie')
-
-# Voeg een nieuwe naam toe zonder de originele gegevensstructuur te wijzigen
-new_names = original_names + ('David',)
-print("Originele namen:", original_names)
-print("Nieuwe namen:", new_names)
