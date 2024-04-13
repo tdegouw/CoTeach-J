f29b1ddb42e77e1877f24f361dc4e68e5bed6a93
Ted de Gouw <ted@hastur.nl>
2024-04-13 08:47:31 +0200

    garbage collection demo

--- Slides/voorbeeld6.py
+++ Slides/voorbeeld6.py
@@ -1,3 +1,5 @@
+import gc
+
 class Monster:
 
     def __init__(self, naam: str):
@@ -9,10 +11,16 @@ class Monster:
     def __del__(self):
         print("En nu is {} niet meer!".format(self.naam))
 
+def print_objects():
+    for obj in gc.get_objects():
+        if isinstance(obj, Monster):
+            print(obj)
 
 joep = Monster("Joep")
+print_objects()
 kees = Monster("Kees")
-print(joep)
-print(kees)
-del(joep)
+print_objects()
 del(kees)
+print_objects()
+del(joep)
+print_objects()
